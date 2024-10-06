bf21e42b72376f678b4f7eb753461fbd5f45e281
Fatih Tezcan <fatihtezcan1071@gmail.com>
2024-10-06 19:52:42 +0300

    color adjustment to blue

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,13 +13,18 @@ const config: Config = {
         secondary: "#2d698e",
         // filter for secondary color
         // style={{filter:"invert(27%) sepia(18%) saturate(2272%) hue-rotate(167deg) brightness(89%) contrast(101%)",}}
-        background: "#fffcf7",
-        accent: "#c26404",
-        lightaccent: "#e87a01",
+        background: "#e1fffe",
+        // background: "#fffcf7",
+        accent: "#2d698e",
+        // accent: "#c26404",
+        lightaccent: "#2d698e",
+        // lightaccent: "#e87a01",
       },
       backgroundImage: {
         gradient_background:
-          "linear-gradient(to right, #fffcf7, #f9efdf, #fffcf7)",
+          "linear-gradient(to right, #e3f4ff, #d4e6f2, #e3f4ff)",
+        // gradient_background:
+        //   "linear-gradient(to right, #fffcf7, #f9efdf, #fffcf7)",
         gradient_flexbox_background:
           "linear-gradient(90deg, rgba(45,105,142,1) 0%, rgba(0,94,182,1) 22%, rgba(45,105,142,1) 100%)",
         cyan_to_darkblue: "linear-gradient(to right, #06b6d4, #1e3a8a)", // Cyan to Dark Blue gradient
